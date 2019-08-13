0efa96533d6ff00771f147a34e341cba020baa78
kgandira <kgandira@gmail.com>
2019-08-13 17:59:15 +0700

    remove unwanted navigation

--- templates/header-default.php
+++ templates/header-default.php
@@ -6,7 +6,7 @@
     include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 ?>
 <header class="menu-stick sticky-top header-one <?php if ( $enabled_header_top ) { echo 'no-header-top'; } ?>">
-    <div class="container-fluid">
+    <div class="container-fluidx">
         <div class="d-flex">
             <?php if( $enabled_header_top == 0 ): ?>
             <div class="header-top">
@@ -154,18 +154,6 @@
                     ?>
                     <a href="<?php echo esc_url(home_url( '/' ));?>"><img src="<?php echo esc_url($store_logo);?>" alt="Logo"></a>
                 </div><!--End .logo-->
-                <div id="main-nav">
-                    <?php
-                        wp_nav_menu(array(
-                            'theme_location' => 'main-menu',
-                            'menu_class'    => 'col-md-12 col-sm-12 hidden-xs',
-                            'menu_id'       => 'main-navigation',
-                            'container'     => '',
-                        ));
-                    ?>
-    
-    
-                </div>
             </div>
             <span class="humberger-button d-flex d-sm-none">
                 <button>
