f3bd1a0b6cf3b36ef2cac8a489e67b567ab8ac77
kgandira <kgandira@gmail.com>
2019-08-22 21:06:59 +0700

    test

--- vc_templates/be_ultimateform.php
+++ vc_templates/be_ultimateform.php
@@ -10,7 +10,7 @@ $id_form  =  "form_id_".rand(1111,9999);
 <?php if ( !is_user_logged_in() ) {
     if( !empty($form_title) ) {
         ?>
-        <h4 style="color: #0a0a0a;text-align: left" class="vc_custom_heading font-intro-bold vc_custom_1566450676803"><? echo esc_html($form_title) ?> </h4>
+        <h4 style="color: #0a0a0a;text-align: left; margin-left: 50px;" class="vc_custom_heading font-intro-bold vc_custom_1566450676803"><? echo esc_html($form_title) ?> </h4>
     <?php } ?>
         <div class="sc-ultimate-form <?php echo esc_attr( $form_class ) ?>" id="<?php echo esc_attr($id_form); ?>">
             <?php echo do_shortcode($content); ?>
