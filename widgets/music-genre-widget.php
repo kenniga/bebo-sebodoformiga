700a31c6554f58cfad8337614b9b6798da155504
Kenni Gandira Alamsyah <kennialamsyah@Kenni-Alamsyah.local>
2019-09-01 21:38:11 +0700

    tes

--- widgets/music-genre-widget.php
+++ widgets/music-genre-widget.php
@@ -1,6 +1,78 @@
 <?php
 class Custom_WC_Widget_Music_Movie_Genres extends WC_Widget {
 
+	/**
+	 * Category ancestors.
+	 *
+	 * @var array
+	 */
+	public $cat_ancestors;
+
+	/**
+	 * Current Category.
+	 *
+	 * @var bool
+	 */
+	public $current_cat;
+
+	/**
+	 * Constructor.
+	 */
+	function __construct() {
+		$this->widget_cssclass    = 'woocommerce widget_product_categories';
+		$this->widget_description = __( 'A list or dropdown of product categories.', 'woocommerce' );
+		$this->widget_id          = 'woocommerce_product_categories_new';
+		$this->widget_name        = __( 'Product Categories New', 'woocommerce' );
+		$this->settings           = array(
+			'title'              => array(
+				'type'  => 'text',
+				'std'   => __( 'Product categories', 'woocommerce' ),
+				'label' => __( 'Title', 'woocommerce' ),
+			),
+			'orderby'            => array(
+				'type'    => 'select',
+				'std'     => 'name',
+				'label'   => __( 'Order by', 'woocommerce' ),
+				'options' => array(
+					'order' => __( 'Category order', 'woocommerce' ),
+					'name'  => __( 'Name', 'woocommerce' ),
+				),
+			),
+			'dropdown'           => array(
+				'type'  => 'checkbox',
+				'std'   => 0,
+				'label' => __( 'Show as dropdown', 'woocommerce' ),
+			),
+			'count'              => array(
+				'type'  => 'checkbox',
+				'std'   => 0,
+				'label' => __( 'Show product counts', 'woocommerce' ),
+			),
+			'hierarchical'       => array(
+				'type'  => 'checkbox',
+				'std'   => 1,
+				'label' => __( 'Show hierarchy', 'woocommerce' ),
+			),
+			'show_children_only' => array(
+				'type'  => 'checkbox',
+				'std'   => 0,
+				'label' => __( 'Only show children of the current category', 'woocommerce' ),
+			),
+			'hide_empty'         => array(
+				'type'  => 'checkbox',
+				'std'   => 0,
+				'label' => __( 'Hide empty categories', 'woocommerce' ),
+			),
+			'max_depth'          => array(
+				'type'  => 'text',
+				'std'   => '',
+				'label' => __( 'Maximum depth', 'woocommerce' ),
+			),
+		);
+
+		parent::__construct();
+	}
+
   function widget( $args, $instance ) {
 		global $wp_query, $post;
 
