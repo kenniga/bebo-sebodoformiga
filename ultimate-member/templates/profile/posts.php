bc24ba94fe2f7d1ec0cb5417a7b390dfe3700133
Kenni Alamsyah <kenni.alamsyah@tiket.com>
2020-02-23 08:44:16 +0700

    finish

--- ultimate-member/templates/profile/posts.php
+++ ultimate-member/templates/profile/posts.php
@@ -17,11 +17,11 @@ if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
 				UM()->shortcodes()->load_template( 'profile/posts-single' );
 			}
 
-			if ( $count_posts > 10 ) { ?>
+			if ( $count_posts > 12 ) { ?>
 				<div class="um-load-items">
 					<a href="javascript:void(0);" class="um-ajax-paginate um-button" data-hook="um_load_posts"
 					   data-author="<?php echo esc_attr( um_get_requested_user() ); ?>" data-page="1"
-					   data-pages="<?php echo esc_attr( ceil( $count_posts / 10 ) ); ?>">
+					   data-pages="<?php echo esc_attr( ceil( $count_posts / 12 ) ); ?>">
 						<?php _e( 'load more posts', 'ultimate-member' ); ?>
 					</a>
 				</div>
